6594333efb93e17675a965b1d082ba9ab4651e1d
Aditya Dwivedi <dwivediaditya0961@gmail.com>
2023-10-21 19:08:49 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="bg-[#20B2AA] w-full h-screen">yo</div>
+    </>
+  )
+}
+
+export default App
